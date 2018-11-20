986af5e5e8ef0a5f046b143a328025c0b32de114
Michael Doronin <warrior2031@mail.ru>
2018-11-21 02:09:56 +0400

    wip

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -4,6 +4,9 @@ import javax.persistence.*;
 import java.util.List;
 import java.util.Map;
 
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 @Entity
 @Table(name = "a")
 public class A {
@@ -12,21 +15,26 @@ public class A {
     @Column(name = "a_id")
     long id;
 
-
     @Column(name = "parent_id")
-    Long parent_id;
+    Long parentId;
+
+    @Column(name = "order_by")
+    Long order;
 
     @ManyToOne
     @JoinColumn(name = "parent_id", insertable = false, updatable = false)
     A parent;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "parent_id")
+    @OrderColumn(name = "order_by")
     List<A> children;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "object_id")
-    @Where(clause = "object_name = 'A'")
-    @MapKey(name = "name")
-    Map<String, Field> fields;
+
+    public String toString() {
+        return "{\"id\": " + String.valueOf(id)
+            + ", \"children\": ["
+            + this.children.stream().map(A::toString).collect(Collectors.joining(", "))
+            + "]";
+    }
 }
